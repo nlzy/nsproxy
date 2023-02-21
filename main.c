3fd45b858bdb7d1e5f8ecaa504dee0cd51ee3ca4
NaLan ZeYu <nalanzeyu@gmail.com>
2023-02-22 00:28:27 +0800

    impl sys_now sys_init

--- main.c
+++ main.c
@@ -3,3 +3,5 @@ void sys_check_core_locking(void) {}
 void sys_mark_tcpip_thread(void) {}
 void sys_lock_tcpip_core(void) {}
 void sys_unlock_tcpip_core(void) {}
+void sys_init(void) {}
+int sys_now(void) { return 0; }
