e4dd2aaac8e040fb693fdd4c2a0ce70075d54400
NaLan ZeYu <nalanzeyu@gmail.com>
2023-02-22 00:36:10 +0800

    remove some impl

--- main.c
+++ main.c
@@ -1,7 +1,3 @@
 int main() {}
-void sys_check_core_locking(void) {}
-void sys_mark_tcpip_thread(void) {}
-void sys_lock_tcpip_core(void) {}
-void sys_unlock_tcpip_core(void) {}
 void sys_init(void) {}
 int sys_now(void) { return 0; }
