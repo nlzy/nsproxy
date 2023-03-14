bccc3395e561ddca179017f15677022a0f41b1d5
NaLan ZeYu <nalanzeyu@gmail.com>
2023-03-14 15:28:17 +0800

    fix socks5 streaming transport

--- socks.c
+++ socks.c
@@ -24,19 +24,158 @@ struct conn_socks {
     struct ep_poller io_poller;
     struct epoll_event io_poller_ev;
 
-    char snd_buffer[512];
-    size_t snd_buffer_size;
+    char sndbuf[512];
+    size_t nsndbuf;
 
-    char rcv_buffer[512];
-    size_t rcv_buffer_size;
+    char rcvbuf[512];
+    size_t nrcvbuf;
 };
 
 struct socks5hdr {
     uint8_t ver;
-    uint8_t cr;
-    uint8_t rsv;
-    uint8_t atyp;
+    union {
+        uint8_t cmd;
+        uint8_t rsp;
+    };
+    union {
+        uint8_t rsv;
+        uint8_t frag;
+    };
 };
+
+struct socks5addr {
+    char addr[256];
+    uint16_t port;
+};
+#define SOCKS5_ATYPE_INET4  1
+#define SOCKS5_ATYPE_DOMAIN 3
+#define SOCKS5_ATYPE_INET6  4
+
+#define SOCKS5_CMD_CONNECT  1
+#define SOCKS5_CMD_UDPASSOC 3
+
+ssize_t socks5_hdr_put(char *buffer, size_t size, const struct socks5hdr *hdr)
+{
+    if (sizeof(struct socks5hdr) > size)
+        return -1;
+    memcpy(buffer, hdr, sizeof(struct socks5hdr));
+    return sizeof(struct socks5hdr);
+}
+
+ssize_t socks5_hdr_get(struct socks5hdr *hdr, const char *buffer, size_t size)
+{
+    if (sizeof(struct socks5hdr) > size)
+        return -1;
+    memcpy(hdr, buffer, sizeof(struct socks5hdr));
+    return sizeof(struct socks5hdr);
+}
+
+ssize_t socks5_addr_put(char *buffer, size_t size, const struct socks5addr *ad)
+{
+    size_t offset = 0;
+    struct in_addr in4;
+    struct in6_addr in6;
+    uint8_t atype, alen;
+    const void *aptr;
+    uint16_t portbe = htobe16(ad->port);
+
+    if (inet_pton(AF_INET, ad->addr, &in4) == 1) {
+        atype = SOCKS5_ATYPE_INET4;
+        alen = sizeof(in4);
+        aptr = &in4;
+    } else if (inet_pton(AF_INET6, ad->addr, &in6) == 1) {
+        atype = SOCKS5_ATYPE_INET6;
+        alen = sizeof(in6);
+        aptr = &in6;
+    } else {
+        atype = SOCKS5_ATYPE_DOMAIN;
+        alen = strlen(ad->addr);
+        aptr = ad->addr;
+    }
+
+    if (atype == SOCKS5_ATYPE_DOMAIN) {
+        if (sizeof(atype) + sizeof(alen) + alen + sizeof(portbe) > size)
+            return -1;
+    } else {
+        if (sizeof(atype) + alen + sizeof(portbe) > size)
+            return -1;
+    }
+
+    memcpy(buffer + offset, &atype, sizeof(atype));
+    offset += sizeof(atype);
+
+    if (atype == SOCKS5_ATYPE_DOMAIN) {
+        memcpy(buffer + offset, &alen, sizeof(alen));
+        offset += sizeof(alen);
+    }
+
+    memcpy(buffer + offset, aptr, alen);
+    offset += alen;
+
+    memcpy(buffer + offset, &portbe, sizeof(portbe));
+    offset += sizeof(portbe);
+
+    return offset;
+}
+
+ssize_t socks5_addr_get(struct socks5addr *ad, const char *buffer, size_t size)
+{
+    const char *cur = buffer;
+    struct in_addr in4;
+    struct in6_addr in6;
+    uint8_t atype, alen;
+    uint16_t portbe;
+
+    if (cur - buffer + sizeof(atype) > size)
+        return -1;
+    memcpy(&atype, cur, sizeof(atype));
+    cur += sizeof(atype);
+
+    switch (atype) {
+    case SOCKS5_ATYPE_INET4:
+        if (cur - buffer + sizeof(in4) > size)
+            return -1;
+        memcpy(&in4, cur, sizeof(in4));
+        cur += sizeof(in4);
+
+        inet_ntop(AF_INET, &in4, ad->addr, sizeof(ad->addr));
+        break;
+
+    case SOCKS5_ATYPE_INET6:
+        if (cur - buffer + sizeof(in6) > size)
+            return -1;
+        memcpy(&in6, cur, sizeof(in6));
+        cur += sizeof(in6);
+
+        inet_ntop(AF_INET6, &in6, ad->addr, sizeof(ad->addr));
+        break;
+
+    case SOCKS5_ATYPE_DOMAIN:
+        if (cur - buffer + sizeof(alen) > size)
+            return -1;
+        memcpy(&alen, cur, sizeof(alen));
+        cur += sizeof(alen);
+
+        if (cur - buffer + alen > (ssize_t)size)
+            return -1;
+        memset(ad->addr, 0, sizeof(ad->addr));
+        memcpy(ad->addr, cur, alen);
+        cur += alen;
+        break;
+
+    default:
+        return -1;
+    }
+
+    if (cur - buffer + sizeof(portbe) > size)
+        return -1;
+    memcpy(&portbe, cur, sizeof(portbe));
+    cur += sizeof(portbe);
+
+    ad->port = be16toh(portbe);
+
+    return cur - buffer;
+}
 
 void socks_io_event(struct ep_poller *poller, unsigned int event)
 {
@@ -47,20 +186,65 @@ void socks_io_event(struct ep_poller *poller, unsigned int event)
 void socks_handshake_phase_4(struct ep_poller *poller, unsigned int event)
 {
     struct conn_socks *h = container_of(poller, struct conn_socks, io_poller);
-    char methods[64];
-    ssize_t nread;
+    struct socks5hdr hdr;
+    struct socks5addr ad;
+    ssize_t s, nread;
+    int pass;
 
-    if ((nread = recv(h->sfd, &methods, sizeof(methods), 0)) == -1) {
+    if (event & (EPOLLERR | EPOLLHUP)) {
         h->userev(h->userp, EPOLLERR);
         return;
     }
 
-    if (nread < 10) {
+    if ((nread = recv(h->sfd, h->rcvbuf + h->nrcvbuf,
+                      sizeof(h->rcvbuf) - h->nrcvbuf - 1, MSG_PEEK)) == -1) {
+        if (!is_ignored_skerr(errno)) {
+            perror("recv()");
+            abort();
+        }
+        return;
+    }
+
+    do {
+        ssize_t ret, offset = 0;
+
+        s = nread;
+        pass = 0;
+
+        ret = socks5_hdr_get(&hdr, h->rcvbuf + offset,
+                             h->nrcvbuf + nread - offset);
+        if (ret == -1)
+            break;
+        offset += ret;
+
+        ret = socks5_addr_get(&ad, h->rcvbuf + offset,
+                              h->nrcvbuf + nread - offset);
+        if (ret == -1)
+            break;
+        offset += ret;
+
+        s = offset - h->nrcvbuf;
+        pass = 1;
+    } while (0);
+
+    if ((nread = recv(h->sfd, h->rcvbuf + h->nrcvbuf, s, 0)) != s) {
+        fprintf(stderr, "recv() returned %zd, expected %zd\n", nread, s);
+        abort();
+    }
+    h->nrcvbuf += nread;
+
+    if (h->nrcvbuf == sizeof(h->rcvbuf)) {
         h->userev(h->userp, EPOLLERR);
         return;
     }
 
-    if (methods[0] != 5 && methods[1] != 0) {
+    if (!pass) {
+        if (s == 0)
+            h->userev(h->userp, EPOLLERR);
+        return;
+    }
+
+    if (hdr.ver != 5 || hdr.rsp != 0) {
         h->userev(h->userp, EPOLLERR);
         return;
     }
@@ -77,20 +261,49 @@ void socks_handshake_phase_4(struct ep_poller *poller, unsigned int event)
 void socks_handshake_phase_3(struct ep_poller *poller, unsigned int event)
 {
     struct conn_socks *h = container_of(poller, struct conn_socks, io_poller);
-    char request[] = { 5, 1, 0, 1, 0, 0, 0, 0, 0, 0 };
     ssize_t nsent;
 
-    inet_pton(AF_INET, h->addr, request + 4);
-    request[8] = h->port >> 8 & 0xFF;
-    request[9] = h->port & 0xFF;
-
-    if ((nsent = send(h->sfd, &request, sizeof(request), MSG_NOSIGNAL)) == -1) {
+    if (event & (EPOLLERR | EPOLLHUP)) {
         h->userev(h->userp, EPOLLERR);
         return;
     }
 
-    if (nsent != sizeof(request)) {
-        h->userev(h->userp, EPOLLERR);
+    if (h->nsndbuf == 0) {
+        struct socks5hdr hdr = { .ver = 5, .cmd = SOCKS5_CMD_CONNECT };
+        struct socks5addr ad;
+        ssize_t ret;
+
+        strncpy(ad.addr, h->addr, sizeof(ad.addr));
+        ad.port = h->port;
+
+        ret = socks5_hdr_put(h->sndbuf + h->nsndbuf,
+                             sizeof(h->sndbuf) - h->nsndbuf, &hdr);
+        if (ret == -1) {
+            h->userev(h->userp, EPOLLERR);
+            return;
+        }
+        h->nsndbuf += ret;
+
+        ret = socks5_addr_put(h->sndbuf + h->nsndbuf,
+                              sizeof(h->sndbuf) - h->nsndbuf, &ad);
+        if (ret == -1) {
+            h->userev(h->userp, EPOLLERR);
+            return;
+        }
+        h->nsndbuf += ret;
+    }
+
+    if ((nsent = send(h->sfd, h->sndbuf, h->nsndbuf, MSG_NOSIGNAL)) == -1) {
+        if (!is_ignored_skerr(errno)) {
+            perror("send()");
+            abort();
+        }
+        return;
+    }
+    h->nsndbuf -= nsent;
+
+    if (h->nsndbuf != 0) {
+        memmove(h->sndbuf, h->sndbuf + nsent, h->nsndbuf);
         return;
     }
 
@@ -106,24 +319,37 @@ void socks_handshake_phase_3(struct ep_poller *poller, unsigned int event)
 void socks_handshake_phase_2(struct ep_poller *poller, unsigned int event)
 {
     struct conn_socks *h = container_of(poller, struct conn_socks, io_poller);
-    char methods[64];
     ssize_t nread;
 
-    if ((nread = recv(h->sfd, &methods, sizeof(methods), 0)) == -1) {
+    if (event & (EPOLLERR | EPOLLHUP)) {
         h->userev(h->userp, EPOLLERR);
         return;
     }
 
-    if (nread < 2) {
+    if ((nread = recv(h->sfd, h->rcvbuf + h->nrcvbuf,
+                      sizeof(h->rcvbuf) - h->nrcvbuf, 0)) == -1) {
+        if (!is_ignored_skerr(errno)) {
+            perror("recv()");
+            abort();
+        }
+        return;
+    }
+    h->nrcvbuf += nread;
+
+    if (h->nrcvbuf == 0 || h->nrcvbuf > 2) {
         h->userev(h->userp, EPOLLERR);
         return;
     }
 
-    if (methods[0] != 5 && methods[1] != 0) {
+    if (h->nrcvbuf == 1)
+        return;
+
+    if (h->rcvbuf[0] != 5 || h->rcvbuf[1] != 0) {
         h->userev(h->userp, EPOLLERR);
         return;
     }
 
+    h->nrcvbuf = 0;
     h->io_poller_ev.events = EPOLLOUT;
     h->io_poller.on_epoll_event = &socks_handshake_phase_3;
     if (epoll_ctl(loop_epfd(h->loop), EPOLL_CTL_MOD, h->sfd,
@@ -136,17 +362,34 @@ void socks_handshake_phase_2(struct ep_poller *poller, unsigned int event)
 void socks_handshake_phase_1(struct ep_poller *poller, unsigned int event)
 {
     struct conn_socks *h = container_of(poller, struct conn_socks, io_poller);
-    char methods[] = { 5, 1, 0 };
     ssize_t nsent;
 
-    /* TODO: socks5 password auth */
-    if ((nsent = send(h->sfd, &methods, sizeof(methods), MSG_NOSIGNAL)) == -1) {
+    if (event & (EPOLLERR | EPOLLHUP)) {
         h->userev(h->userp, EPOLLERR);
         return;
     }
 
-    if (nsent != sizeof(methods)) {
-        h->userev(h->userp, EPOLLERR);
+    if (h->nsndbuf == 0) {
+        if (sizeof(h->sndbuf) < 3) {
+            h->userev(h->userp, EPOLLERR);
+            return;
+        }
+        h->sndbuf[h->nsndbuf++] = 5; /* ver */
+        h->sndbuf[h->nsndbuf++] = 1; /* num */
+        h->sndbuf[h->nsndbuf++] = 0; /* no auth */
+    }
+
+    if ((nsent = send(h->sfd, h->sndbuf, h->nsndbuf, MSG_NOSIGNAL)) == -1) {
+        if (!is_ignored_skerr(errno)) {
+            perror("send()");
+            abort();
+        }
+        return;
+    }
+    h->nsndbuf -= nsent;
+
+    if (h->nsndbuf != 0) {
+        memmove(h->sndbuf, h->sndbuf + nsent, h->nsndbuf);
         return;
     }
 
@@ -253,40 +496,47 @@ void socks_evctl(struct sk_ops *handle, unsigned int event, int enable)
 ssize_t socks_send(struct sk_ops *handle, const char *data, size_t size)
 {
     struct conn_socks *h = (struct conn_socks *)handle;
-    char buffer[65535];
     ssize_t nsent;
-    const char *p;
-    size_t s;
-    struct socks5hdr hdr = { 0 };
-    struct sockaddr_in sa;
 
     if (h->io_poller.on_epoll_event != &socks_io_event) {
         return -EAGAIN;
     }
 
     if (h->isudp) {
-        if (size + 10 > sizeof(buffer)) {
-            return -EAGAIN; /* TODO: return TRUNC errno */
+        char buffer[512];
+        struct socks5hdr hdr = { 0 };
+        struct socks5addr addr;
+        size_t offset = 0;
+        ssize_t ret;
+
+        strncpy(addr.addr, h->addr, sizeof(addr.addr));
+        addr.port = h->port;
+
+        if ((ret = socks5_hdr_put(buffer + offset, sizeof(buffer) - offset,
+                                  &hdr)) == -1)
+            return -EAGAIN;
+        offset += ret;
+
+        if ((ret = socks5_addr_put(buffer + offset, sizeof(buffer) - offset,
+                                   &addr)) == -1)
+            return -EAGAIN;
+        offset += ret;
+
+        if (offset + size > 65535 - 8 - 40)
+            return -EAGAIN;
+
+        nsent = send(h->sfd, buffer, offset, MSG_NOSIGNAL | MSG_MORE);
+        if (nsent == -1) {
+            if (is_ignored_skerr(errno)) {
+                nsent = -errno;
+            } else {
+                perror("send()");
+                abort();
+            }
         }
-
-        hdr.atyp = 1;
-        memcpy(buffer, &hdr, sizeof(hdr));
-
-        inet_pton(AF_INET, h->addr, &sa.sin_addr);
-        sa.sin_port = htobe16(h->port);
-        memcpy(buffer + 4, &sa.sin_addr, sizeof(sa.sin_addr));
-        memcpy(buffer + 8, &sa.sin_port, sizeof(sa.sin_port));
-
-        memcpy(buffer + 10, data, size);
-
-        p = buffer;
-        s = size + 10;
-    } else {
-        p = data;
-        s = size;
     }
 
-    nsent = send(h->sfd, p, s, MSG_NOSIGNAL);
+    nsent = send(h->sfd, data, size, MSG_NOSIGNAL);
     if (nsent == -1) {
         if (is_ignored_skerr(errno)) {
             nsent = -errno;
@@ -329,11 +579,22 @@ ssize_t socks_recv(struct sk_ops *handle, char *data, size_t size)
 #endif
 
     if (h->isudp) {
-        if (size < 10) {
-            return -EAGAIN; /* bad */
-        }
-        memmove(data, data + 10, size - 10);
-        nread -= 10;
+        struct socks5hdr hdr;
+        struct socks5addr ad;
+        ssize_t ret, offset = 0;
+
+        ret = socks5_hdr_get(&hdr, data + offset, nread - offset);
+        if (ret == -1)
+            return -EAGAIN;
+        offset += ret;
+
+        ret = socks5_addr_get(&ad, data + offset, nread - offset);
+        if (ret == -1)
+            return -EAGAIN;
+        offset += ret;
+
+        memmove(data, data + offset, nread - offset);
+        nread -= offset;
     }
 
     return nread;
